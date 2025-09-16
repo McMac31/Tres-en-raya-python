fae7ac0b5b920373663e4f93e507438e210165e0
unknown <ikmacampo24>
2025-09-16 09:27:58 +0200

    Version actualizada con Bot incluido

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,9 +1,9 @@
-from pickle import TRUE
+
 import random
-from tkinter import CHAR
 print("Tres en raya")
 jugador = False
 hayGanador = False
+
 
 Tablero = [[" ", " ", " "],
            [" ", " ", " "],
@@ -13,14 +13,12 @@ Tablero = [[" ", " ", " "],
 def ElBot():
     while True:
             PreguntaBot= input("Desea jugar contra el Bot? Y/N ")
-            if PreguntaBot == "Y":
+            if PreguntaBot == "Y" or PreguntaBot== "y":
                 print("Jugando con el bot")
                 return True
-            
-            elif PreguntaBot=="N":
+            elif PreguntaBot =="N" or PreguntaBot== "n":
                 print("Cambiando a modo dos jugadores... ")
-                return True
-                pass
+                return False
             else:
                 print("Ingrese un caracter valido")
 
@@ -32,11 +30,11 @@ def ElBot():
 def TableroBonito():
     print(
         f"    0       1       2\n"
-        f"0  {Tablero[0][0]}   |   {Tablero[0][1]}   |   {Tablero[0][2]}\n"
-        f"  -----------------------\n"
-        f"1  {Tablero[1][0]}   |   {Tablero[1][1]}   |   {Tablero[1][2]}\n"
-        f"  -----------------------\n"
-        f"2  {Tablero[2][0]}   |   {Tablero[2][1]}   |   {Tablero[2][2]}")
+        f"0  {Tablero[0][0]}   ║   {Tablero[0][1]}   ║   {Tablero[0][2]}\n"
+        f"  ═════╬═══════╬═══════\n"
+        f"1  {Tablero[1][0]}   ║   {Tablero[1][1]}   ║   {Tablero[1][2]}\n"
+        f"  ═════╬═══════╬═══════\n"
+        f"2  {Tablero[2][0]}   ║   {Tablero[2][1]}   ║   {Tablero[2][2]}\n")
 
 def verTablero():
     for casilla in Tablero:
@@ -74,24 +72,38 @@ def Turno(jugador):
 def seleccionarSimbolo(jugador):
     if jugador == False:
         simbolo = "X"
-        print("El Turno del jugador 1 fue: ")
+        print("El Turno del jugador 1 fue: \n")
+    elif jugador and BotActivo == True:
+        simbolo="O"
+        print("El turno del Bot fue: \n")
     else:
         simbolo = "O"
-        print("El Turno del jugador 2 fue: ")
+        print("El Turno del jugador 2 fue: \n")
     return simbolo
 
-ElBot()
+BotActivo=ElBot()
+
+
 print("Jugador 1 Empieza:")
 TableroBonito()
 
+
+
 while hayGanador == False:
     try:
+        if jugador and BotActivo:
+            while True:
+                            jugadaX= random.randint(0,2)
+                            jugadaY= random.randint(0,2)
+                            if Tablero[jugadaY][jugadaX]== " ":
+                                break
+        else:
        
-        jugadaX = int(input("Ingrese la posicion X a jugar (0-2): "))
-        jugadaY = int(input("Ingrese la posicion Y a jugar (0-2): "))
+            jugadaX = int(input("Ingrese la posicion X a jugar (0-2): \n"))
+            jugadaY = int(input("Ingrese la posicion Y a jugar (0-2): \n "))
     except ValueError:
-        print("Ingrese un valor numerico")
-        continue
+            print("Ingrese un valor numerico")
+            continue
 
     if jugadaX > 2 or jugadaX < 0 or jugadaY > 2 or jugadaY < 0:
         print("Esa posicion no existe")
@@ -108,7 +120,12 @@ while hayGanador == False:
 
     if LineaHorizontal() or LineaVertical() or LineaDiagonal():
         hayGanador = True
-        ganador = "Primer jugador (X)" if simbolo == "X" else "Segundo jugador (O)"
+        if simbolo == "X":
+            ganador="Primer jugador (X) "
+        elif BotActivo and simbolo=="O":
+            ganador="Bot "
+        else:
+            ganador="Segundo jugador (O)"
         print(f"¡Ha ganado el {ganador}!")
         TableroBonito()
         break
